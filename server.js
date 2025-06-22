c3ff1f1f4512d35ac9d5a48b437c2e3d13b4eabb
nisargcp18 <nisargcp18@gmail.com>
2025-06-22 17:30:10 +0530

    In this commit I have completed the navbar.

---
+++ server.js
@@ -0,0 +1,15 @@
+(async function main() {
+    const express = require('express');
+    const mongoose = require('mongoose');
+    const app = express();
+    const port = 3000;
+    await mongoose.connect('mongodb://localhost:27017/AsyncSync');
+    
+    app.get('/', (req, res)=>{
+        res.sendFile('./public/homePage/home.html', {root: __dirname})
+    })
+    
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+})()
